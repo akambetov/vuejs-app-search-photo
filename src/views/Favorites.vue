823d3f758fb3917097a027ed0b83ac0b0a017896
Almat Kambetov <almat_kambetov@mail.ru>
2021-01-10 23:30:32 +0600

    views/Favorites.vue page

---
+++ src/views/Favorites.vue
@@ -0,0 +1,19 @@
+<template>
+  <ph-photos v-if="favorites" :photos="favorites" />
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import PhPhotos from '@/components/Photos'
+export default {
+  name: 'PhFavorites',
+  components: {
+    PhPhotos
+  },
+  computed: {
+    ...mapState({
+      favorites: state => state.favorites.favorites
+    })
+  }
+}
+</script>
