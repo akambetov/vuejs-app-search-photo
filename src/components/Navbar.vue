25570413048de13a91dc6eaadc3f37d4719c1537
Almat Kambetov <almat_kambetov@mail.ru>
2021-01-28 20:07:57 +0600

    добавил класс "active" для пунктов навигации в src/components/Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,18 +2,31 @@
   <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
     <div>
       <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <router-link :to="{ name: 'home' }" class="nav-link">
+        <li class="nav-item">
+          <router-link
+            :to="{ name: 'home' }"
+            class="nav-link"
+            active-class="active"
+            exact
+          >
             Home
           </router-link>
         </li>
         <li class="nav-item">
-          <router-link :to="{ name: 'favorites' }" class="nav-link">
+          <router-link
+            :to="{ name: 'favorites' }"
+            class="nav-link"
+            active-class="active"
+          >
             Favorites
           </router-link>
         </li>
-        <li class="nav-item">
-          <router-link :to="{ name: 'history' }" class="nav-link">
+        <li class="nav-item" active-class="active" exact>
+          <router-link
+            :to="{ name: 'history' }"
+            class="nav-link"
+            active-class="active"
+          >
             History
           </router-link>
         </li>
